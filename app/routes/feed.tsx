8bf2f3da69d3d36431c877f25be2f693c13a95c5
entropy622 <1450431082@qq.com>
2025-05-02 20:30:56 +0800

    加上overflow

--- app/routes/feed.tsx
+++ app/routes/feed.tsx
@@ -10,7 +10,7 @@ export function meta(_args: Route.MetaArgs) {
 
 export default function Feed() {
   return (
-    <div className="h-screen bg-base-200">
+    <div className="h-screen bg-base-200 overflow-auto">
       <FeedPage></FeedPage>
     </div>
   );
