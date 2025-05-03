1f8f5be70b5bf231f72be3af18ce949f7a4a63cc
entropy622 <1450431082@qq.com>
2025-05-03 15:37:09 +0800

    渲染文件名加上时间戳，避免覆盖；webgal可渲染被踢出的角色与骰娘；webgal渲染提速

--- app/webGAL/game.ts
+++ app/webGAL/game.ts
@@ -5,13 +5,3 @@ export async function editScene(game: string, scene: string, content: string) {
   const path = `games/${game}/game/scene/${scene}.txt`;
   return (await terreApis.manageGameControllerEditTextFile({ path, textFile: content })).data;
 }
-
-export async function createPreview(roomId: number) {
-  return (
-    await terreApis.manageGameControllerCreateGame({
-      gameDir: `preview_${roomId}`,
-      gameName: `preview_${roomId}`,
-      templateDir: "WebGAL Black",
-    })
-  ).data;
-}
