188631c26e40f4c962348dc1cdb71d3b25f5b77c
Ita_Hloks <Wilin2306@outlook.com>
2025-07-10 23:19:21 +0800

    feat: 加入了很有意思的字数超出警告

--- app/components/profile/editProfileWindow.tsx
+++ app/components/profile/editProfileWindow.tsx
@@ -15,17 +15,21 @@ export default function EditProfileWindow({ onClose }: { onClose?: () => void })
   const user = userQuery.data?.data;
 
   const updateUserInfoMutation = useUpdateUserInfoMutation();
-
+  // Shake it!!!
+  const [errorShakeKey, setErrorShakeKey] = useState(0);
   const [username, setUsername] = useState(user?.username || "");
   const [userDescription, setUserDescription] = useState(user?.description || "");
   const [newAvtarUrl, setNewAvtarUrl] = useState(user?.avatar || undefined);
   const handleSave = () => {
-    if (userDescription.length <= DESCRIPTION_MAX) {
+    if (userDescription.length <= DESCRIPTION_MAX && username.length <= 30) {
       if (onClose) {
         onClose();
       }
       const avatarUrl = (newAvtarUrl && newAvtarUrl !== "") ? newAvtarUrl : user?.avatar;
       updateUserInfoMutation.mutate({ ...user, username, avatar: avatarUrl, description: userDescription } as UserInfoResponse);
+    }
+    else {
+      setErrorShakeKey(prev => prev + 1);
     }
   };
   const handleCancel = () => {
@@ -64,25 +68,35 @@ export default function EditProfileWindow({ onClose }: { onClose?: () => void })
               </div>
             </ImgUploaderWithCopper>
             {/* 用户名称 */}
-            <div className="w-full max-w-xs flex items-center gap-2">
-              <label className="label">
+            <div className="w-full max-w-xs flex gap-2 flex-col">
+              <div className="label">
                 <span className="label-text text-lg font-semibold">修改昵称</span>
-              </label>
-              <input
-                type="text"
-                value={username}
-                onChange={e => setUsername(e.target.value)}
-                placeholder="请输入新昵称"
-                className="input input-bordered input-lg w-full focus:ring-2 focus:ring-primary focus:border-transparent"
-              />
+                <input
+                  type="text"
+                  value={username}
+                  onChange={e => setUsername(e.target.value)}
+                  placeholder="请输入新昵称"
+                  className="input input-bordered input-lg w-full focus:ring-2 focus:ring-primary focus:border-transparent"
+                />
+              </div>
+              <div
+                key={errorShakeKey}
+                style={{
+                  animation: username.length > 30 ? "quick-shake 0.3s ease-in-out" : "none",
+                }}
+                className={`text-right text-sm overflow-hidden transition-all duration-300 ease-in-out ${
+                  username.length > 30
+                    ? "text-error max-h-8 opacity-100 translate-y-0"
+                    : "max-h-0 opacity-0 translate-y-2"
+                }`}
+              >
+                呜啊...名字最长只能30个字呢...
+              </div>
             </div>
             {/* 个人描述 */}
             <div className="w-full max-w-xs flex flex-col gap-1">
-              {" "}
               {/* 改为垂直布局 */}
               <label htmlFor="userDescription" className="label cursor-pointer">
-                {" "}
-                {/* 添加 htmlFor 和交互提示 */}
                 <span className="label-text text-lg font-medium">修改描述</span>
               </label>
               <textarea
@@ -101,9 +115,16 @@ export default function EditProfileWindow({ onClose }: { onClose?: () => void })
               }`}
               >
                 {userDescription.length}
-                /253
+                {" "}
+                /
+                {" "}
+                {DESCRIPTION_MAX}
               </div>
               <div
+                key={errorShakeKey}
+                style={{
+                  animation: username.length > 30 ? "quick-shake 0.3s ease-in-out" : "none",
+                }}
                 className={`text-right text-sm overflow-hidden transition-all duration-300 ease-in-out ${
                   userDescription.length > DESCRIPTION_MAX
                     ? "text-error max-h-8 opacity-100 translate-y-0"
