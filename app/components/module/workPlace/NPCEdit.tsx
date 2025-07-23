01ae12ce303b02a2622b16f0f9ff6564892ca21a
甘雨的狗 <super_ganyudegou@qq.com>
2025-07-23 10:34:39 +0800

    Module edit (#93)
    * 修复了角色的bug

--- app/components/module/workPlace/NPCEdit.tsx
+++ app/components/module/workPlace/NPCEdit.tsx
@@ -136,7 +136,7 @@ export default function NPCEdit({ role }: NPCEditProps) {
         <div className="card-body">
           <div className="flex items-center gap-8">
             {/* 头像 */}
-            <CharacterCopper setDownloadUrl={() => { }} setCopperedDownloadUrl={handleAvatarChange} fileName={uniqueFileName}>
+            <CharacterCopper setDownloadUrl={() => { }} setCopperedDownloadUrl={handleAvatarChange} fileName={uniqueFileName} scene={4}>
               <div className="avatar cursor-pointer group flex items-center justify-center w-[50%] min-w-[120px] md:w-48">
                 <div className="rounded-xl ring-primary ring-offset-base-100 w-full ring ring-offset-2 relative">
                   <div className="absolute inset-0 bg-black/0 group-hover:bg-black/10 transition-all flex items-center justify-center z-1" />
