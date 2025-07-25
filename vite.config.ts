8ea1d43f0e6a65ed653ec00b3b5275471089cd03
entropy622 <1450431082@qq.com>
2025-07-25 10:43:58 +0800

    feat:引入了PWA，但似乎没生效

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
@@ -9,6 +10,38 @@ export default defineConfig({
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      injectRegister: "auto",
+      manifest: {
+        name: "tuan-chat",
+        short_name: "tuan-chat",
+        description: "tuan-chat",
+        theme_color: "#0042ff",
+        icons: [{
+          src: "/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "favicon",
+        }, {
+          src: "/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "favicon",
+        }, {
+          src: "/apple-touch-icon.png",
+          sizes: "180x180",
+          type: "image/png",
+          purpose: "apple touch icon",
+        }],
+        background_color: "#f0e7db",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+      },
+    }),
   ],
   base: "/",
   resolve: {
@@ -21,6 +54,7 @@ export default defineConfig({
   },
   server: {
     port: 5177,
+    host: "0.0.0.0",
   },
 
 });
