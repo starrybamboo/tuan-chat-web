a453d8ae9dd1b051bd3675fb6e41f25b233084e3
entropy622 <1450431082@qq.com>
2025-08-02 09:49:05 +0800

    feat：右键添加表情

--- app/utils/getImgSize.ts
+++ app/utils/getImgSize.ts
@@ -3,8 +3,8 @@
  * @param img - 图片源，可以是 File 对象或图片的 URL 字符串。
  * @returns 一个 Promise，解析为一个包含图片宽高信息
  */
-export async function getImageSize(img: File | string): Promise<{ width: number; height: number }> {
-  const defaultSize = { width: -1, height: -1 };
+export async function getImageSize(img: File | string): Promise<{ width: number; height: number; size: number }> {
+  const defaultSize = { width: -1, height: -1, size: -1 };
   let objectUrl = "";
 
   try {
@@ -26,6 +26,7 @@ export async function getImageSize(img: File | string): Promise<{ width: number;
     return {
       width: imageElement.naturalWidth,
       height: imageElement.naturalHeight,
+      size: blob.size,
     };
   }
   catch (error) {
