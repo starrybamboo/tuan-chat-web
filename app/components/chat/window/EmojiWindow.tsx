d3fd628cd9afe9e74a25d506126edc2d9c246f47
XuHanYang <281423846@qq.com>
2025-08-05 14:53:32 +0800

    feat: 删表情

--- app/components/chat/window/EmojiWindow.tsx
+++ app/components/chat/window/EmojiWindow.tsx
@@ -1,20 +1,21 @@
-import type { Emoji } from "../../../../api";
+import type { Emoji } from "api/models/Emoji";
 import { ImgUploader } from "@/components/common/uploader/imgUploader";
 import { UploadUtils } from "@/utils/UploadUtils";
-import { useCreateEmojiMutation, useGetUserEmojisQuery } from "api/hooks/emojiQueryHooks";
+import { useCreateEmojiMutation, useDeleteEmojiMutation, useGetUserEmojisQuery } from "api/hooks/emojiQueryHooks";
+import { useState } from "react";
 
 export default function EmojiWindow({ onChoose }:
 {
   onChoose: (emoji: Emoji) => void; // 选择表情的回调函数
 }) {
-  // 获取用户自定义表情列表
   const { data: emojisData } = useGetUserEmojisQuery();
   const emojiList = Array.isArray(emojisData?.data) ? emojisData.data : [];
+  const deleteEmojiMutation = useDeleteEmojiMutation();
+  const [deleteButtonVisible, setDeleteButtonVisible] = useState(false);
 
   // 新增表情
   const createEmojiMutation = useCreateEmojiMutation();
   const uploadUtils = new UploadUtils();
-
   const handleAddEmoji = async (newImg: File) => {
     if (!newImg) {
       return;
@@ -31,20 +32,34 @@ export default function EmojiWindow({ onChoose }:
     createEmojiMutation.mutate(emojiCreateRequest);
   };
 
+  // 删除表情
+  const onDelete = (emojiId: number) => {
+    deleteEmojiMutation.mutate(emojiId);
+  };
+
   return (
-    <div className="p-1 pb-0 w-full">
+    <div className="w-full">
       {/* 显示表情 */}
-      <div className="grid grid-cols-4 mb-2">
+      <div className="grid grid-cols-5 mb-2 max-h-80 p-1 overflow-y-auto overflow-x-hidden">
+        {/* 添加新表情 */}
+        <ImgUploader setImg={(newImg) => { handleAddEmoji(newImg); }}>
+          <div className="aspect-square cursor-pointer hover:bg-base-200 rounded-lg p-1 transition-transform">
+            <div className="flex items-center justify-center h-full text-base-content/70">
+              <span className="text-2xl">+</span>
+            </div>
+          </div>
+        </ImgUploader>
+
         {emojiList.length === 0
           ? (
-              <div className="col-span-4 text-center text-base-content/70">暂无表情包</div>
+              <div className="col-span-5 text-center text-base-content/70 h-20 flex items-center justify-center">暂无表情包</div>
             )
           : (
               emojiList.map(emoji => (
                 <div
                   key={emoji.emojiId}
                   onClick={() => { onChoose(emoji); }}
-                  className="aspect-square cursor-pointer hover:bg-base-200 rounded-lg p-2 hover:scale-105 transition-transform"
+                  className={`aspect-square cursor-pointer rounded-lg p-1 transition-transform relative ${deleteButtonVisible ? "" : "hover:scale-105"}`}
                 >
                   <img
                     src={emoji.imageUrl}
@@ -52,17 +67,34 @@ export default function EmojiWindow({ onChoose }:
                     className="w-full h-full object-contain"
                     loading="lazy"
                   />
+                  {/* 删除按钮 */}
+                  {deleteButtonVisible && (
+                    <button
+                      type="button"
+                      className="cursor-pointer absolute top-0 right-0 rounded-full size-5 flex items-center justify-center bg-gray-300 text-gray-700 hover:bg-gray-400 transition-colors"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        onDelete(emoji.emojiId || -1);
+                      }}
+                    >
+                      ×
+                    </button>
+                  )}
                 </div>
               ))
             )}
-        {/* 添加新表情 */}
-        <ImgUploader setImg={(newImg) => { handleAddEmoji(newImg); }}>
-          <div className="aspect-square cursor-pointer hover:bg-base-200 rounded-lg p-2 hover:scale-105 transition-transform">
+        {emojiList.length !== 0 && (
+          <div
+            className="aspect-square cursor-pointer hover:bg-base-200 rounded-lg p-1 transition-transform"
+            onClick={() => {
+              setDeleteButtonVisible(!deleteButtonVisible);
+            }}
+          >
             <div className="flex items-center justify-center h-full text-base-content/70">
-              <span className="text-2xl">+</span>
+              <span className="text-xl">×</span>
             </div>
           </div>
-        </ImgUploader>
+        )}
       </div>
       {/* 选择表情包列表 */}
       <div className="flex">
