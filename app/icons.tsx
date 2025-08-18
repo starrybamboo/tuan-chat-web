5760fbdeb7eed3886fdcab4b9eab1706a2e735b2
Ita_Hloks <Wilin2306@outlook.com>
2025-08-18 14:40:09 +0800

    fix: 解决合并冲突

--- app/icons.tsx
+++ app/icons.tsx
@@ -1183,3 +1183,24 @@ export function BarChartOutlineIcon(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+// 柱状统计图标
+// 其他关键词：数据可视化，图表，分析，投票
+export function SearchFilled(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M469.297 439.13L347.982 317.816C370.466 288.907 384 252.707 384 213.334c0-94.104-76.562-170.667-170.666-170.667S42.667 119.23 42.667 213.334S119.23 384 213.334 384c39.373 0 75.573-13.534 104.481-36.018l121.316 121.315zm-255.963-97.796c-70.584 0-128-57.417-128-128c0-70.584 57.416-128 128-128c70.583 0 128 57.416 128 128c0 70.583-57.417 128-128 128"
+      >
+      </path>
+    </svg>
+  );
+}
