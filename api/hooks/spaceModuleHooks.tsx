97968b7e70321f639b3c1f75efb851ffe9a0b5c1
lmagnitude <13505638011@163.com>
2025-09-07 16:42:51 +0800

    feat：一键创建空间并导入模组，线索系统初步实装

--- api/hooks/spaceModuleHooks.tsx
+++ api/hooks/spaceModuleHooks.tsx
@@ -45,3 +45,16 @@ export function useGetRoomItemsQuery(roomId: number) {
     enabled: roomId >= 0,
   })
 }
+
+/**
+ * 获取当前房间的地点
+ * @param roomId 房间ID
+ */
+export function useGetRoomLocationsQuery(roomId: number) {
+  return useQuery({
+    queryKey: ['roomLocations', roomId],
+    queryFn: () => tuanchat.spaceModuleController.roomLocation(roomId),
+    staleTime: 10000, // 缓存时间
+    enabled: roomId != -1
+  })
+}
