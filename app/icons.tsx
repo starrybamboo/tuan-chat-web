8590549869ff143d8170a9e5c4bd8930c6fd730e
甘雨的狗 <super_ganyudegou@qq.com>
2025-09-14 18:40:11 +0800

    Module recreate (#254)
    * fix: 再次修复icon

--- app/icons.tsx
+++ app/icons.tsx
@@ -1476,6 +1476,22 @@ export function ArrowBackThickFill(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+// 下载图标
+export function SharpDownload(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      <path fill="currentColor" d="M19 9h-4V3H9v6H5l7 7zM5 18v2h14v-2z"></path>
+    </svg>
+  );
+}
+
 // 链接图标
 export function Link(props: SVGProps<SVGSVGElement>) {
   return (
