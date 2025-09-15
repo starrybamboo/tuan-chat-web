4c34289eca3119653ca9e5f2e9f7ce1b9ed72230
entropy622 <1450431082@qq.com>
2025-09-15 22:32:52 +0800

    feat：更新openApi文档

--- api/services/RoomRoleControllerService.ts
+++ api/services/RoomRoleControllerService.ts
@@ -2,10 +2,8 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-import type { ApiResultListStageEntityResponse } from '../models/ApiResultListStageEntityResponse';
 import type { ApiResultListUserRole } from '../models/ApiResultListUserRole';
 import type { ApiResultVoid } from '../models/ApiResultVoid';
-import type { ModuleRoleAddRequest } from '../models/ModuleRoleAddRequest';
 import type { RoomRoleAddRequest } from '../models/RoomRoleAddRequest';
 import type { RoomRoleDeleteRequest } from '../models/RoomRoleDeleteRequest';
 import type { CancelablePromise } from '../core/CancelablePromise';
@@ -13,51 +11,7 @@ import type { BaseHttpRequest } from '../core/BaseHttpRequest';
 export class RoomRoleControllerService {
     constructor(public readonly httpRequest: BaseHttpRequest) {}
     /**
-     * 添加房间的模组角色
-     * @param requestBody
-     * @returns ApiResultVoid OK
-     * @throws ApiError
-     */
-    public addModuleRole(
-        requestBody: ModuleRoleAddRequest,
-    ): CancelablePromise<ApiResultVoid> {
-        return this.httpRequest.request({
-            method: 'POST',
-            url: '/capi/room/role/npc',
-            body: requestBody,
-            mediaType: 'application/json',
-            errors: {
-                400: `Bad Request`,
-                405: `Method Not Allowed`,
-                429: `Too Many Requests`,
-                500: `Internal Server Error`,
-            },
-        });
-    }
-    /**
-     * 删除房间模组角色
-     * @param requestBody
-     * @returns ApiResultVoid OK
-     * @throws ApiError
-     */
-    public deleteModuleRole(
-        requestBody: ModuleRoleAddRequest,
-    ): CancelablePromise<ApiResultVoid> {
-        return this.httpRequest.request({
-            method: 'DELETE',
-            url: '/capi/room/role/npc',
-            body: requestBody,
-            mediaType: 'application/json',
-            errors: {
-                400: `Bad Request`,
-                405: `Method Not Allowed`,
-                429: `Too Many Requests`,
-                500: `Internal Server Error`,
-            },
-        });
-    }
-    /**
-     * 增加房间的玩家角色
+     * 增加房间的角色
      * @param requestBody
      * @returns ApiResultVoid OK
      * @throws ApiError
@@ -79,7 +33,7 @@ export class RoomRoleControllerService {
         });
     }
     /**
-     * 删除房间的玩家角色
+     * 删除房间的角色
      * @param requestBody
      * @returns ApiResultVoid OK
      * @throws ApiError
@@ -103,12 +57,12 @@ export class RoomRoleControllerService {
     /**
      * 获取房间的所有模组角色
      * @param roomId
-     * @returns ApiResultListStageEntityResponse OK
+     * @returns ApiResultListUserRole OK
      * @throws ApiError
      */
     public roomModuleRole(
         roomId: number,
-    ): CancelablePromise<ApiResultListStageEntityResponse> {
+    ): CancelablePromise<ApiResultListUserRole> {
         return this.httpRequest.request({
             method: 'GET',
             url: '/capi/room/role/npc/list',
