3229b1f2dfe460c8fc58b3f9f3963897951002aa
降星驰 <107794984+starrybamboo@users.noreply.github.com>
2025-09-16 13:29:44 +0800

    Chat status (#275)
    * feat: 添加了更多的输入状态
    
    * feat: 可以看见自己的输入状态了，优化了状态机的变更逻辑

--- app/components/chat/roomWindow.tsx
+++ app/components/chat/roomWindow.tsx
@@ -531,7 +531,7 @@ export function RoomWindow({ roomId, spaceId }: { roomId: number; spaceId: numbe
                 />
                 {/* 底部工具栏 */}
                 {/* 状态显示条 */}
-                <ChatStatusBar roomId={roomId} userId={userId} webSocketUtils={webSocketUtils} />
+                <ChatStatusBar roomId={roomId} userId={userId} webSocketUtils={webSocketUtils} excludeSelf={false} />
                 <ChatToolbar
                   sideDrawerState={sideDrawerState}
                   setSideDrawerState={setSideDrawerState}
