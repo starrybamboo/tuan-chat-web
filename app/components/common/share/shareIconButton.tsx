470b1f522602ffb22f0fbc71bfd09a676b34433f
真白 <185361175+moeyue23@users.noreply.github.com>
2025-09-18 23:14:23 +0800

    Feat/share function (#296)
    * feat: 分享组件适配移动端
    
    * feat: 帖子可以被分享
    
    * fix: 修复eslint错误

--- app/components/common/share/shareIconButton.tsx
+++ app/components/common/share/shareIconButton.tsx
@@ -32,11 +32,11 @@ export default function ShareIconButton({
 
     const instance = toastWindow(
       () => (
-        <div className="overflow-y-auto space-y-4 h-[40vh] w-[30vw] flex flex-col items-center justify-center p-4">
+        <div className="overflow-y-auto space-y-4 h-[15vh] w-[50vw] sm:h-[15vh] sm:w-[55vh] md:h-[15vh] md:w-[40vh] lg:h-[30vh] lg:w-[50vh] max-w-full flex flex-col items-center justify-center">
           <h2 className="text-xl font-bold">分享方式</h2>
           <div className="flex gap-4 mt-4">
-            <SavePictureButton targetRef={targetRef} qrLink={qrLink} />
-            <CopyLinkButton title={title} />
+            <SavePictureButton targetRef={targetRef} qrLink={qrLink} className="flex-1 min-w-0" />
+            <CopyLinkButton title={title} className="flex-1 min-w-0" />
           </div>
         </div>
       ),
