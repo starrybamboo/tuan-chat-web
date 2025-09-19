567e14e12cd7a0cbdbefe80e558df816896c8877
XuHanYang <281423846@qq.com>
2025-09-20 00:42:39 +0800

    chore

--- app/routes/module/create.tsx
+++ app/routes/module/create.tsx
@@ -1,10 +1,10 @@
 import type { Route } from "../+types/home";
-import ModuleCreate2 from "@/components/module/create/ModuleCreate2";
+import ModuleCreateMain from "@/components/module/create/ModuleCreateMain";
 
 export function meta(_args: Route.MetaArgs) {
   return [
-    { title: "创建模组物品" },
-    { name: "description", content: "Create your own module items here" },
+    { title: "创建模组" },
+    { name: "description", content: "Create your own module here" },
   ];
 }
 
@@ -12,19 +12,7 @@ export default function ModuleCreate() {
   return (
     <div className="bg-base-100 overflow-auto">
       {/* 这里添加创建模组的表单 */}
-      <ModuleCreate2 />
+      <ModuleCreateMain />
     </div>
   );
 }
-
-// export default function ModuleCreate() {
-//   return (
-//     <div className="bg-base-100">
-//       <div className="mx-auto max-w-[1280px] px-4 py-[10px]">
-//         <h1 className="text-2xl font-bold mb-4">创建模组</h1>
-//         {/* 这里添加创建模组的表单 */}
-//         <ModuleForm />
-//       </div>
-//     </div>
-//   );
-// }
