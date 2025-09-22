87c0d7916ae8663045044d296b37118145603dc1
Ita_Hloks <115126342+Ita-Hloks@users.noreply.github.com>
2025-09-22 08:58:55 +0800

    feat: 个人主页核心页面添加路由

---
+++ app/routes/profile/profile.tsx
@@ -0,0 +1,17 @@
+import type { Route } from "../+types/home";
+import ProfilePage from "@/components/profile/profilePage";
+
+export function meta({ params }: Route.MetaArgs) {
+  return [
+    { title: `用户 ${params.userId} 的主页 - tuan-chat` },
+    { name: "description", content: `查看用户 ${params.userId} 的个人主页` },
+  ];
+}
+
+export default function Profile() {
+  return (
+    <div className="bg-base-200 h-full w-full overflow-y-auto overflow-x-hidden">
+      <ProfilePage />
+    </div>
+  );
+}
