15d12da33098e44fa90cfd19f2e753365c2b3f55
lmagnitude <13505638011@163.com>
2025-10-05 20:40:57 +0800

    优化线索消息样式 (#351)

--- app/components/chat/smallComponents/clueMessage.tsx
+++ app/components/chat/smallComponents/clueMessage.tsx
@@ -20,20 +20,20 @@ function ClueMessage({ messageResponse }: { messageResponse: ChatMessageResponse
           <h4 className="text-lg font-semibold text-info">{clueMessage.name}</h4>
 
           {/* 线索图片 + 描述 */}
-          <div className="flex gap-3 flex-wrap">
+          <div className="overflow-hidden">
             {/* 线索图片 */}
             {clueMessage.img && (
-              <div className="w-32 h-32 rounded overflow-hidden flex-shrink-0 border border-base-300">
+              <div className="float-left mr-3 mb-3 w-32 rounded overflow-hidden border border-base-300">
                 <BetterImg
                   src={clueMessage.img}
-                  className="w-full h-full object-cover cursor-pointer"
+                  className="w-full h-full object-cover cursor-pointer aspect-square"
                   key={`${clueMessage.name}-img`}
                 />
               </div>
             )}
 
-            {/* 线索描述 - 使用MarkdownMentionViewer渲染 */}
-            <div className="text-base text-base-content/90 flex-1 min-w-[200px]">
+            {/* 线索描述 */}
+            <div className="text-base text-base-content/90">
               <MarkdownMentionViewer
                 markdown={clueMessage.description || "无描述信息"}
                 enableHoverPreview={true}
