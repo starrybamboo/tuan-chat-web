044f891fdd46d43488d53240411e2378fe07a8ed
entropy622 <1450431082@qq.com>
2025-10-08 11:12:02 +0800

    fix：之前交错了一个文件

--- app/components/chat/roomWindow.tsx
+++ app/components/chat/roomWindow.tsx
@@ -13,10 +13,10 @@ import ChatInputArea from "@/components/chat/chatInputArea";
 import ChatStatusBar from "@/components/chat/chatStatusBar";
 import ChatToolbar from "@/components/chat/chatToolbar";
 import CommandPanel from "@/components/chat/commandPanel";
-import { ExpressionChooser } from "@/components/chat/expressionChooser";
 import useChatInputStatus from "@/components/chat/hooks/useChatInputStatus";
 import { useChatHistory } from "@/components/chat/indexedDB/useChatHistory";
 import SearchBar from "@/components/chat/inlineSearch";
+import DNDMap from "@/components/chat/map/DNDMap";
 import { RoomContext } from "@/components/chat/roomContext";
 import InitiativeList from "@/components/chat/sideDrawer/initiativeList";
 import RoomRoleList from "@/components/chat/sideDrawer/roomRoleList";
@@ -38,9 +38,9 @@ import {
   BaselineArrowBackIosNew,
 } from "@/icons";
 import { getImageSize } from "@/utils/getImgSize";
-import { getScreenSize } from "@/utils/getScreenSize";
 // *** 导入新组件及其 Handle 类型 ***
 
+import { getScreenSize } from "@/utils/getScreenSize";
 import { UploadUtils } from "@/utils/UploadUtils";
 import React, { use, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { toast } from "react-hot-toast";
@@ -670,8 +670,7 @@ export function RoomWindow({ roomId, spaceId }: { roomId: number; spaceId: numbe
             onWidthChange={setMapDrawerWidth}
             maxWidth={window.innerWidth - 700}
           >
-            {/* <DNDMap></DNDMap> */}
-            <ExpressionChooser roleId={10012} handleExpressionChange={() => {}} handleRoleChange={() => {}}></ExpressionChooser>
+            <DNDMap></DNDMap>
           </OpenAbleDrawer>
           <OpenAbleDrawer
             isOpen={sideDrawerState === "clue"}
