970dda53d30389e38106dce0593cdf7d9385c034
lmagnitude <13505638011@163.com>
2025-10-30 20:49:39 +0800

    Lmagnitude (#395)
    * fix：限制线索名称展示字数
    
    * fix：再限短点

--- app/components/chat/sideDrawer/clueListForPL.tsx
+++ app/components/chat/sideDrawer/clueListForPL.tsx
@@ -438,7 +438,7 @@ export default function ClueListForPL({ onSend }: { onSend: (clue: ClueMessage) 
                                           </svg>
                                         </div>
                                       )}
-                                  <span className="text-sm font-medium text-neutral-800 dark:text-neutral-100 max-w-28 truncate">
+                                  <span className="text-sm font-medium text-neutral-800 dark:text-neutral-100 max-w-24 truncate">
                                     {clue.name}
                                   </span>
                                 </div>
