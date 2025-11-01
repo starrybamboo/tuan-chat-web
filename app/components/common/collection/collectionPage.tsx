c26b230feb75ef3aed58b2ffe581e59b183b1926
moeyue23 <185361175+moeyue23@users.noreply.github.com>
2025-11-01 17:04:56 +0800

    feat: 增加创建收藏夹功能

--- app/components/common/collection/collectionPage.tsx
+++ app/components/common/collection/collectionPage.tsx
@@ -1,48 +1,102 @@
 import CollectionList from "@/components/common/collection/collectionList";
 import CollectionPreview from "@/components/common/collection/collectionPreview";
 import CollectionSearchBar from "@/components/common/collection/collectionSearchBar";
+import toastWindow from "@/components/common/toastWindow/toastWindow";
+import { ImgUploader } from "@/components/common/uploader/imgUploader";
+import { useCreateCollectionListMutation, useGetListCollectionsQuery } from "api/hooks/collectionQueryHooks";
 import { useState } from "react";
 
-// --- 模拟收藏数据 ---
-interface MockCollectionItem {
-  collectionId: number;
+function NewCollectionForm({ onClose }: { onClose: () => void }) {
+  const [form, setForm] = useState({
+    collectionListName: "",
+    description: "",
+    isPublic: false,
+    resourceListType: "1",
+    coverImageUrl: "",
+  });
+  // const [file, setFile] = useState<File | null>(null);
 
-  resourceId: 2 | 3 | 4; // 2=帖子, 3=模组, 4=评论
-  comment: string;
-  createTime: string;
-}
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, files } = e.target; // e.target.name 和 state 对应
+    setForm(prev => ({ ...prev, [name]: value }));
+    // 文件类型的输入单独处理
+    if (type === "file" && files && files.length > 0) {
+      setForm(prev => ({ ...prev, [name]: files[0] }));
+    }
+    else {
+      setForm(prev => ({ ...prev, [name]: value }));
+    }
+  };
 
-// 三种类型随机分配
-const resourceTypes: Array<2 | 3 | 4> = [2, 3, 4];
+  // const handleFile = (selectedFile: File) => {
 
-const mockCollections: MockCollectionItem[] = Array.from({ length: 40 }).map((_, i) => ({
-  collectionId: i + 1,
-  resourceId: resourceTypes[Math.floor(Math.random() * resourceTypes.length)],
-  comment: `这是第 ${i + 1} 个收藏`,
-  createTime: new Date(
-    Date.now() - Math.floor(Math.random() * 1000 * 60 * 60 * 24 * 30), // 最近30天内随机时间
-  ).toISOString(),
-}));
-
-// 可以在组件中直接使用 mockCollections 渲染
+  // };
+  const createMutation = useCreateCollectionListMutation();
+  const handleSubmit = () => {
+    createMutation.mutate(form, {
+      onSuccess: () => {
+        onClose();
+      },
+      onError: (error) => {
+        console.error("创建失败", error);
+      },
+    });
+    onClose();
+  };
+  return (
+    <div>
+      <div className="text-center text-2xl font-bold">新建收藏夹</div>
+      {/* <label className="btn btn-primary cursor-pointer">
+        选择文件
+        <input
+          type="file"
+          name="coverImageUrl"
+          value={form.coverImageUrl}
+          className="hidden"
+          onChange={handleChange}
+        />
+      </label>
+      <span className="max-w-64 block text-sm text-gray-500 truncate">
+        {file ? file.name : "未选择文件"}
+      </span> */}
+      <ImgUploader setImg={() => {}}>
+        <div className="text-base-content/60 hover:text-primary transition-colors p-1 rounded-full hover:bg-base-200 cursor-pointer">
+          上传封面
+        </div>
+      </ImgUploader>
+      <div className="text-lg font-medium">名称</div>
+      <input className="border p-2 rounded focus:outline-none focus:ring-0" type="text" name="collectionListName" value={form.collectionListName} onChange={handleChange} />
+      <div className="text-lg font-medium">描述</div>
+      <input className="border p-2 rounded focus:outline-none focus:ring-0" type="text" name="description" value={form.description} onChange={handleChange} />
+      <div className="p-2">
+        <label>
+          <input className="w-5 h-5 radio radio-primary" type="radio" name="isPublic" checked={form.isPublic === true} onChange={() => setForm(prev => ({ ...prev, isPublic: true }))} />
+          公开
+        </label>
+        <label>
+          <input className="w-5 h-5 radio radio-primary" type="radio" name="isPublic" checked={form.isPublic === false} onChange={() => setForm(prev => ({ ...prev, isPublic: false }))} />
+          不公开
+        </label>
+      </div>
+      <button className="block mx-auto px-4 py-2 btn btn-primary rounded w-40" type="button" onClick={handleSubmit}>提交</button>
+    </div>
+  );
+};
 
 export default function CollectionPage() {
-  if (typeof window === "undefined")
-    return null; // ⛔️ SSR 时跳过
-  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [selectedListId, setSelectedListId] = useState(1); // 默认收藏夹 id
 
-  const isLoading = false;
-  const isError = false;
-  const collections = mockCollections;
-  // 获取当前收藏夹内容
-  // const PAGE_SIZE = 20;
-  // const { data, isLoading, isError } = useGetListCollectionsQuery({
-  //   pageSize: PAGE_SIZE,
-  //   collectionListId: selectedListId,
-  // });
-  // const collections = data?.pages.flatMap(page => page?.data?.list) ?? [];
+  const handleAddCollection = () => toastWindow(close => (<NewCollectionForm onClose={close} />));
 
+  // 获取当前收藏夹内容
+  const PAGE_SIZE = 20;
+  const { data, isLoading, isError } = useGetListCollectionsQuery({
+    pageSize: PAGE_SIZE,
+    collectionListId: selectedListId,
+  });
+  const collections = data?.pages.flatMap(page => page?.data?.list) ?? [];
+
+  // 添加收藏夹
   return (
     <div className="flex flex-col min-h-screen bg-base-200/70 dark:bg-base-200 p-4 lg:p-6 gap-6 font-sans">
       {/* 顶部搜索栏 */}
@@ -52,16 +106,22 @@ export default function CollectionPage() {
 
       {/* 主体区域 */}
       <div className="flex flex-col lg:flex-row gap-6 lg:gap-10 mt-16">
-        {/* 左侧收藏夹列表 */}
         <div className="card lg:fixed flex flex-col w-full lg:w-1/4 bg-base-100 shadow-lg border border-gray-300 dark:border-black p-4">
           <CollectionList
             selectedId={selectedListId}
             onSelect={setSelectedListId}
+            onAddCollection={handleAddCollection}
           />
         </div>
 
         {/* 右侧收藏内容 */}
-        <div className="flex-1 lg:ml-[28%]">
+        <div className="flex-1 flex-col lg:ml-[28%]">
+          <ul className="flex gap-2">
+            <li>全部</li>
+            <li>帖子</li>
+            <li>评论</li>
+            <li>模组</li>
+          </ul>
           <div className="grid grid-cols-3 auto-rows-[10px] gap-5">
             {isLoading
               ? (
@@ -81,7 +141,7 @@ export default function CollectionPage() {
                       collectionId={c?.collectionId}
                       resourceId={c?.resourceId}
                       collectionTypeId={c?.resourceId}
-                      collectTime={c?.createTime}
+                      collectTime={String(c?.createTime)}
                     />
                   ))}
           </div>
